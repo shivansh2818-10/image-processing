f616b1edeeaa9b4121fc8289069d769d0e945ede
Branton Davis <bluecamel@gmail.com>
2021-01-29 15:30:01 -0600

    Add comment about boost filesystem.

--- stitcher/include/airmap/panorama.h
+++ stitcher/include/airmap/panorama.h
@@ -14,6 +14,9 @@ namespace filesystem {
 /**
  * @brief The path class is a basic ersatz for a std::filesystem (C++17) or boost::filesystem
  * that doesn't require linking anything
+ * 
+ * boost::filesystem::path is used in other parts of the project, but this header
+ * is used in 
  */
 class path {
     public:
